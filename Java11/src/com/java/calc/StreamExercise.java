707e6a64c8e7336729acb6517ef3259862505ead
Janmejaya <bapun00@gmail.com>
2022-12-02 11:39:43 +0530

    all files

---
+++ Java11/src/com/java/calc/StreamExercise.java
@@ -0,0 +1,21 @@
+package com.java.calc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class StreamExcercise{
+    public static void main(String[] args) {
+    		//Input
+    		
+    		List<String> names=new ArrayList<String>();
+    		names.add("Jack");
+    		names.add("John");
+    		names.add("Jill");
+    		names.add("Jim");
+    		names.add("Garry");
+    		names.add("Frank");
+    		
+    		// Write your code here
+    }
+    
+}
